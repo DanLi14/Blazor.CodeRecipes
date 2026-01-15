fd7b1b25a7b55c3b371444d955b3e8f010b86c97
DanielLi <daniel1490.li@gmail.com>
2026-01-15 16:27:52 +0000

    Recipe06

---
+++ BlazorCookbook.App/BlazorCookbook.App.Client/wwwroot/BlazorCookbook.App.Client.lib.module.js
@@ -0,0 +1,16 @@
+﻿
+/*designed to intercept brower's copy event and overwrite clipboard data'*/
+export function afterWebStarted(blazor)
+{
+    blazor.registerCustomEventType('preventcopy', {
+        browserEventName: 'copy',
+        createEventArgs: event => {
+            event.clipboardData.setData('text/plain', '---');
+            event.preventDefault();
+            return {
+                stamp: new Date()
+            };
+        }
+    }); 
+}
+
