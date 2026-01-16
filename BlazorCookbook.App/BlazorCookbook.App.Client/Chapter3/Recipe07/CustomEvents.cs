46ef74615aed15c7eac3b4668def5e42db52240b
DanielLi <daniel1490.li@gmail.com>
2026-01-16 16:49:50 +0000

    Recipe07

---
+++ BlazorCookbook.App/BlazorCookbook.App.Client/Chapter3/Recipe07/CustomEvents.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorCookbook.App.Client.Chapter3.Recipe07
+{
+    [EventHandler("onpreventcopy", typeof(PreventedCopyEventArgs))] /*the EventHandler attribute informs Razor of our custom onpreventcopy event*/
+    public static class EventHandlers { }
+
+    public class PreventedCopyEventArgs : EventArgs
+    {
+        public DateTime Stamp { get; init; }
+    }
+}
